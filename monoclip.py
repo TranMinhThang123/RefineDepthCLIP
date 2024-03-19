1c75b777176ecfa5fb91f466f75a748661d79fdb
TranMinhThang123 <tranminhthang291102@gmail.com>
2024-03-19 16:06:41 +0000

    clear code version

--- monoclip.py
+++ monoclip.py
@@ -53,37 +53,36 @@ class AdapterLayer(nn.Module):
         return x
 
 
+class UpSampleBlock(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.layers = nn.Sequential()
+
+
 # CLIP for Monocular Depth Estimation
 class MonoCLIP(nn.Module):
-    def __init__(self):
+    def __init__(self, decode_type:str):
         super(MonoCLIP, self).__init__()
         self.bins = len(depth_classes)
 
+        assert decode_type in ["upsample","deconv"]," Only supper decode type as upsample or deconv "
+        self.decode_type = decode_type
         self.clip, _ = clip.load(clip_vis) # load pretrained clip encoder
         self.ResNet = self.clip.visual
         self.text_f = zeroshot_classifier(depth_classes, obj_classes, depth_templates, self.clip) # init text feature
+        self.upsample_layer = nn.Upsample(scale_factor=2,mode="bilinear",align_corners=True)
 
         self.text_f.requires_grad = False
         for param in self.ResNet.parameters():
             param.require_grad = False
 
-        self.adapter1 = AdapterLayer(c_in=1024).to(self.clip.dtype)
-        self.adapter2 = AdapterLayer(c_in=1024,reduction=2)
-        self.adapter3 = AdapterLayer(c_in=1024,reduction=1)
-        self.adapter4 = AdapterLayer(c_in=1024,reduction=1/2)
-
-        self.bin_list1 = nn.Parameter(torch.rand(1,7),requires_grad=True).unsqueeze(-1).unsqueeze(-1)
-        self.bin_list2 = nn.Parameter(torch.rand(1,7),requires_grad=True).unsqueeze(-1).unsqueeze(-1)
-        self.bin_list3 = nn.Parameter(torch.rand(1,7),requires_grad=True).unsqueeze(-1).unsqueeze(-1)
-        self.bin_list4 = nn.Parameter(torch.rand(1,7),requires_grad=True).unsqueeze(-1).unsqueeze(-1)
+        self.adapter_list = [AdapterLayer(c_in=1024, reduction=4/(2**i)) for i in range(4)]
+        self.bin_list = [nn.Parameter(torch.rand(1,7),requires_grad=True).unsqueeze(-1).unsqueeze(-1) for i in range(4)]
+        self.size_list = [(120,160),(60,80),(30,40),(15,20)]
+        self.channel_list = [256,512,1024,2048]
 
 
-
-    def forward(self, x):
-        print("*"*150)
-        # img_f = self.clip.encode_image(x).reshape(1,2048,300).permute(0,2,1) # B, HW, C
-        # img_f = img_f / img_f.norm(dim=-1, keepdim=True) # normalize img_f
-        print(self.text_f.shape)
+    def compute_depth_map(self,x):
         def stem(x):
             for conv, bn in [(self.ResNet.conv1, self.ResNet.bn1), (self.ResNet.conv2, self.ResNet.bn2), (self.ResNet.conv3, self.ResNet.bn3)]:
                 x = self.ResNet.relu(bn(conv(x)))
@@ -96,56 +95,35 @@ class MonoCLIP(nn.Module):
         feature_map2 = self.ResNet.layer2(feature_map1)
         feature_map3 = self.ResNet.layer3(feature_map2)
         feature_map4 = self.ResNet.layer4(feature_map3)
-        print(feature_map1.shape,end="=>")
-        print(feature_map2.shape,end="=>")
-        print(feature_map3.shape,end="=>")
-        print(feature_map4.shape)
-
-        feature_map1 = feature_map1.reshape(1,256,120*160)
-        feature_map1 = feature_map1.permute(0,2,1)
-        feature_map1 = feature_map1/feature_map1.norm(dim=-1,keepdim=True)
-        prompt1 = self.adapter1(self.text_f)
-        print(prompt1.size())
-        depth_map1 = feature_map1@prompt1
-        depth_map1 = depth_map1.reshape(-1,self.bins,120,160)
-        depth_map1 = F.softmax(depth_map1,dim=1)
-        print("depth map 1 shape: ",depth_map1.shape)
-        print("feature map 1 shape: ",feature_map1.shape)
-
-        feature_map2 = feature_map2.reshape(1,512,60*80)
-        feature_map2 = feature_map2.permute(0,2,1)
-        feature_map2 = feature_map2/feature_map2.norm(dim=-1,keepdim=True)
-        prompt2 = self.adapter2(self.text_f)
-        print(prompt2.size())
-        depth_map2 = feature_map2@prompt2
-        depth_map2 = depth_map2.reshape(-1,self.bins,60,80)
-        depth_map2 = F.softmax(depth_map2,dim=1)
-        print("depth map 2 shape: ",depth_map2.shape)
-        print("feature map 2 shape: ",feature_map2.shape)
 
 
-        feature_map3 = feature_map3.reshape(1,1024,30*40)
-        feature_map3 = feature_map3.permute(0,2,1)
-        feature_map3 = feature_map3/feature_map3.norm(dim=-1,keepdim=True)
-        prompt3 = self.adapter3(self.text_f)
-        print(prompt3.size())
-        depth_map3 = feature_map3@prompt3
-        depth_map3 = depth_map3.reshape(-1,self.bins,30,40)
-        depth_map3 = F.softmax(depth_map3,dim=1)
-        print("depth map 3 shape: ",depth_map3.shape)
-        print("feature map 3 shape: ",feature_map3.shape)
+        feature_map_list = [feature_map1,feature_map2,feature_map3,feature_map4]
+        feature_map_list = [feature_map_list[i].reshape(1,self.channel_list[i],self.size_list[i][0]*self.size_list[i][1]).permute(0,2,1) for i in range(4)]
+        feature_map_list = [fea/fea.norm(dim=-1,keepdim=True) for fea in feature_map_list]
+        prompts_list = [self.adapter_list[i](self.text_f) for i in range(4)]
 
-        feature_map4 = feature_map4.reshape(1,2048,15*20)
-        feature_map4 = feature_map4.permute(0,2,1)
-        feature_map4 = feature_map4/feature_map4.norm(dim=-1,keepdim=True)
-        prompt4 = self.adapter4(self.text_f)
-        print(prompt4.size())
-        depth_map4 = feature_map4@prompt4
-        depth_map4 = depth_map4.reshape(-1,self.bins,15,20)
-        depth_map4 = F.softmax(depth_map4,dim=1)
-        depth_map4 = depth_map4*self.bin_list4
-        print("depth map 4 shape: ",depth_map4.shape)
-        print("feature map 4 shape: ",feature_map4.shape)
+
+        depth_map_list = [feature_map_list[i]@prompts_list[i] for i in range(4)]
+        depth_map_list = [depth_map_list[i].reshape(-1,self.bins,*self.size_list[i]) for i in range(4)]
+        depth_map_list = [F.softmax(depth_map_list[i],dim=1)*self.bin_list[i] for i in range(4)]
+        for i in depth_map_list:
+            print(i.shape)
+
+        return depth_map_list
+
+
+    def forward(self, x):
+        
+        depth_map1,depth_map2,depth_map3,depth_map4 = self.compute_depth_map(x)
+        if self.decode_type == "upsample":
+            output = self.upsample_layer(depth_map4)
+            output = self.upsample_layer(torch.cat)
+        else:
+            pass
+
+
+
+    
 
         return img_f
 
